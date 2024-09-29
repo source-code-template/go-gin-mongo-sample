84da80fc6f9dc3b1dd20533e0c1b22823a2d48c2
Duc Nguyen <minhduc140583@gmail.com>
2024-09-29 23:30:54 +0700

    Format code

--- internal/app/route.go
+++ internal/app/route.go
@@ -17,13 +17,13 @@ func Route(ctx context.Context, g *gin.Engine, cfg Config) error {
 	userPath := g.Group("/users")
 	{
 		userPath.GET("", app.User.All)
+		userPath.GET("/search", app.User.Search)
+		userPath.POST("/search", app.User.Search)
 		userPath.GET("/:id", app.User.Load)
 		userPath.POST("", app.User.Create)
 		userPath.PUT("/:id", app.User.Update)
 		userPath.PATCH("/:id", app.User.Patch)
 		userPath.DELETE("/:id", app.User.Delete)
-		userPath.GET("/search", app.User.Search)
-		userPath.POST("/search", app.User.Search)
 	}
 
 	return nil
--- internal/user/handler/handler.go
+++ internal/user/handler/handler.go
@@ -7,15 +7,15 @@ import (
 	"reflect"
 
 	"github.com/core-go/core"
-	s "github.com/core-go/search"
+	"github.com/core-go/search"
 	"github.com/gin-gonic/gin"
 
 	"go-service/internal/user/model"
-	sv "go-service/internal/user/service"
+	"go-service/internal/user/service"
 )
 
 type UserHandler struct {
-	service     sv.UserService
+	service     service.UserService
 	Validate    func(context.Context, interface{}) ([]core.ErrorMessage, error)
 	Error       func(context.Context, string, ...map[string]interface{})
 	Map         map[string]int
@@ -23,10 +23,10 @@ type UserHandler struct {
 	FilterIndex int
 }
 
-func NewUserHandler(service sv.UserService, logError func(context.Context, string, ...map[string]interface{}), validate func(context.Context, interface{}) ([]core.ErrorMessage, error)) *UserHandler {
+func NewUserHandler(service service.UserService, logError func(context.Context, string, ...map[string]interface{}), validate func(context.Context, interface{}) ([]core.ErrorMessage, error)) *UserHandler {
 	userType := reflect.TypeOf(model.User{})
 	_, jsonMap, _ := core.BuildMapField(userType)
-	paramIndex, filterIndex := s.BuildAttributes(reflect.TypeOf(model.UserFilter{}))
+	paramIndex, filterIndex := search.BuildAttributes(reflect.TypeOf(model.UserFilter{}))
 	return &UserHandler{service: service, Validate: validate, Map: jsonMap, Error: logError, ParamIndex: paramIndex, FilterIndex: filterIndex}
 }
 
@@ -48,7 +48,7 @@ func (h *UserHandler) Load(c *gin.Context) {
 
 	user, err := h.service.Load(c.Request.Context(), id)
 	if err != nil {
-		h.Error(c.Request.Context(), fmt.Sprintf("Error to get user %s: %s", id, err.Error()))
+		h.Error(c.Request.Context(), fmt.Sprintf("Error to get user '%s': %s", id, err.Error()))
 		c.String(http.StatusInternalServerError, core.InternalServerError)
 		return
 	}
@@ -207,7 +207,7 @@ func (h *UserHandler) Delete(c *gin.Context) {
 
 	res, err := h.service.Delete(c.Request.Context(), id)
 	if err != nil {
-		h.Error(c.Request.Context(), fmt.Sprintf("Error to delete user %s: %s", id, err.Error()))
+		h.Error(c.Request.Context(), fmt.Sprintf("Error to delete user '%s': %s", id, err.Error()))
 		c.String(http.StatusInternalServerError, core.InternalServerError)
 		return
 	}
@@ -219,19 +219,19 @@ func (h *UserHandler) Delete(c *gin.Context) {
 }
 
 func (h *UserHandler) Search(c *gin.Context) {
-	filter := model.UserFilter{Filter: &s.Filter{}}
-	err := s.Decode(c.Request, &filter, h.ParamIndex, h.FilterIndex)
+	filter := model.UserFilter{Filter: &search.Filter{}}
+	err := search.Decode(c.Request, &filter, h.ParamIndex, h.FilterIndex)
 	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
-	offset := s.GetOffset(filter.Limit, filter.Page)
+	offset := search.GetOffset(filter.Limit, filter.Page)
 	users, total, err := h.service.Search(c.Request.Context(), &filter, filter.Limit, offset)
 	if err != nil {
 		h.Error(c.Request.Context(), fmt.Sprintf("Error to to search %v: %s", filter, err.Error()))
 		c.String(http.StatusInternalServerError, core.InternalServerError)
 		return
 	}
-	c.JSON(http.StatusOK, &s.Result{List: &users, Total: total})
+	c.JSON(http.StatusOK, &search.Result{List: &users, Total: total})
 }
